b0709a7c741630c4f639a64435473b269fad835b
korbeldaniel <korbeldaniel@kubuntu-pc>
2017-10-17 21:20:54 +0200

    Clean up

--- src/main/java/pl/korbeldaniel/erpe/server/dao/ContactDao.java
+++ src/main/java/pl/korbeldaniel/erpe/server/dao/ContactDao.java
@@ -22,9 +22,6 @@ import java.util.Optional;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import pl.korbeldaniel.erpe.client.shared.entity.Contact;
 
 /**
